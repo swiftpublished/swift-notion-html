7e7c565bc0fd12084893efebee96108a4ca9a4f3
shahrukhalam <alam.shahrukh786@gmail.com>
2024-06-12 10:44:40 +0530

    basic page test

--- Sources/Html/HTML.swift
+++ Sources/Html/HTML.swift
@@ -1,1 +1,63 @@
 import HTMLDSL
+import NotionParsing
+
+func html(for page: Page) -> some View {
+    Document {
+        HTML {
+            Head {
+                Title("Notion Test Article")
+            }
+
+            Body {
+                Headings(page.properties.title.text, type: .h1)
+
+                for block in page.content?.blocks ?? [] {
+                    switch block.type {
+                    case .paragraph(let paragraph):
+                        Paragraphs(richTexts: paragraph.richTexts)
+                    case .bulletedListItem:
+                        preconditionFailure("Not Handled")
+                    }
+                }
+            }
+        }
+    }
+}
+
+extension Paragraphs {
+    init(richTexts: [RichText]) {
+        let richTextsElement = richTexts.map(HTMLRichText.init).map(\.body).joined()
+        self.init(richTextsElement)
+    }
+}
+
+struct HTMLRichText: HTMLBodyTextContentView {
+    let body: String
+
+    let newLine: NewLine = .none
+
+    let tag = Tag.empty
+    var attributes = [Attribute]()
+
+    let text: String
+
+    init(_ richText: RichText) {
+        self.text = richText.type.description
+
+        var body = text
+        if richText.annotations.bold {
+            body = body.bold
+        }
+        if richText.annotations.italic {
+            body = body.italic
+        }
+        if richText.annotations.strikethrough {
+            body = body.delete
+        }
+        if richText.annotations.underline {
+            body = body.underline
+        }
+
+        self.body = body
+    }
+}
--- Tests/HtmlTests/HTMLTests.swift
+++ Tests/HtmlTests/HTMLTests.swift
@@ -1,12 +1,164 @@
 import XCTest
 @testable import NotionHtml
+import NotionParsing
 
 final class HTMLTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
+    func testSimpleParagraph() throws {
+        let page: Page = .paragraph([
+            .text("This is a Paragraph")
+        ])
 
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+        let document = html(for: page)
+
+        let expected = """
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+        <title> Notion Test Article </title>
+        </head>
+        <body>
+        <h1> Notion Test Article Title </h1>
+        <p>
+        This is a Paragraph
+        </p>
+        </body>
+        </html>
+        """
+
+        XCTAssertEqual(document.element, expected)
+    }
+
+    func testBoldParagraph() throws {
+        let page: Page = .paragraph([
+            .text("This is a Paragraph", bold: true)
+        ])
+
+        let document = html(for: page)
+
+        let expected = """
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+        <title> Notion Test Article </title>
+        </head>
+        <body>
+        <h1> Notion Test Article Title </h1>
+        <p>
+        <b>This is a Paragraph</b>
+        </p>
+        </body>
+        </html>
+        """
+
+        XCTAssertEqual(document.element, expected)
+    }
+
+    func testBoldAndItalicParagraph() throws {
+        let page: Page = .paragraph([
+            .text("This is a Paragraph", bold: true, italic: true)
+        ])
+
+        let document = html(for: page)
+
+        let expected = """
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+        <title> Notion Test Article </title>
+        </head>
+        <body>
+        <h1> Notion Test Article Title </h1>
+        <p>
+        <i><b>This is a Paragraph</b></i>
+        </p>
+        </body>
+        </html>
+        """
+
+        XCTAssertEqual(document.element, expected)
+    }
+
+    func testSimpleParagraphWithPartBold() throws {
+        let page: Page = .paragraph([
+            .text("This is "),
+            .text("bold", bold: true)
+        ])
+
+        let document = html(for: page)
+
+        let expected = """
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+        <title> Notion Test Article </title>
+        </head>
+        <body>
+        <h1> Notion Test Article Title </h1>
+        <p>
+        This is <b>bold</b>
+        </p>
+        </body>
+        </html>
+        """
+
+        XCTAssertEqual(document.element, expected)
+    }
+
+    func testSimpleParagraphWithPartBoldAndItalic() throws {
+        let page: Page = .paragraph([
+            .text("This is "),
+            .text("bold & italic", bold: true, italic: true)
+        ])
+
+        let document = html(for: page)
+
+        let expected = """
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+        <title> Notion Test Article </title>
+        </head>
+        <body>
+        <h1> Notion Test Article Title </h1>
+        <p>
+        This is <i><b>bold & italic</b></i>
+        </p>
+        </body>
+        </html>
+        """
+
+        XCTAssertEqual(document.element, expected)
+    }
+}
+
+private extension Page {
+    static func paragraph(_ richTexts: [RichText]) -> Self {
+        let title = Page.Properties.Title(text: "Notion Test Article Title")
+        let properties = Page.Properties(title: title)
+
+        let paragraph = Block.Paragraph(richTexts: richTexts, children: nil)
+        let block = Block(id: "101", hasChildren: false, type: .paragraph(paragraph), children: nil)
+        let content = Content(nextCursor: nil, hasMore: false, blocks: [block])
+
+        return Page(
+            id: UUID(uuidString: "59833787-2cf9-4fdf-8782-e53db20768a5")!,
+            lastEditedTime: DateFormatter.iso8601Notion.date(from: "2020-03-17T19:10:04.968Z")!,
+            properties: properties,
+            content: content
+        )
+    }
+}
+
+private extension RichText {
+    static func text(_ text: String, bold: Bool = false, italic: Bool = false) -> Self {
+        let text = RichText.Types.Text(content: text, link: nil)
+        let annotations = RichText.Annotations(
+            bold: bold,
+            italic: italic,
+            strikethrough: false,
+            underline: false,
+            code: false
+        )
+        return RichText(type: .text(text), annotations: annotations)
     }
 }
