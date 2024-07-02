438ee11be353f786c81baee38cfa7b282c924e9f
Shahrukh Alam <alam.shahrukh786@gmail.com>
2024-07-02 10:21:57 +0530

    basic page content test

--- Tests/HTMLTests/PageTests.swift
+++ Tests/HTMLTests/PageTests.swift
@@ -19,4 +19,24 @@ final class PageTests: XCTestCase {
 
         XCTAssertEqual(body.element, expected)
     }
+
+    func test_basic_content() throws {
+        let page: Page = .paragraphContent(
+            title: [.text("Title")],
+            paragraph: [.text("Paragraph")]
+        )
+
+        let body = htmlBody(for: page)
+
+        let expected = """
+        <div class="notion-page">
+        <h1 class="notion-heading1"> Title </h1>
+        <p class="notion-paragraph">
+        Paragraph
+        </p>
+        </div>
+        """
+
+        XCTAssertEqual(body.element, expected)
+    }
 }
