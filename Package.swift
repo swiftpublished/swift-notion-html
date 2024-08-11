721b219ad9edd74b68a9b988bdeb22bcaa5a44ef
Shahrukh Alam <alam.shahrukh786@gmail.com>
2024-08-11 13:21:09 +0530

    uses public repo on CI

--- Package.swift
+++ Package.swift
@@ -15,10 +15,6 @@ let package = Package(
             targets: ["NotionHTML"]
         )
     ],
-    dependencies: [
-        .package(path: "../HTMLDSL"),
-        .package(path: "../swift-notion-parsing")
-    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
@@ -40,3 +36,19 @@ let package = Package(
         )
     ]
 )
+
+/// Local Development Setup
+/// 
+/// We can set `CI_ENVIRONMENT` in `fly.io` to fetch from Remote
+if ProcessInfo.processInfo.environment["CI_ENVIRONMENT"] == "YES" {
+    package.dependencies += [
+        // 📃 A HTML page renderer for shahrukh's article website
+        .package(url: "https://github.com/shahrukhalam/HTMLDSL", branch: "main"),
+        .package(url: "https://github.com/swiftpublished/swift-notion-parsing.git", branch: "main")
+    ]
+} else {
+    package.dependencies += [
+        .package(path: "../HTMLDSL"),
+        .package(path: "../swift-notion-parsing")
+    ]
+}
