da7204c01a026727c8ddf9e302b95e62726bfa29
Shahrukh Alam <alam.shahrukh786@gmail.com>
2024-09-15 13:57:38 +0530

    moves title to a separate container

--- Sources/HTML/HTMLBody.swift
+++ Sources/HTML/HTMLBody.swift
@@ -11,12 +11,6 @@ public struct Config {
 
 public func htmlBody(for page: Page, with config: Config) -> some HTMLBodyContentView {
     Div {
-        Headings(
-            richTexts: page.properties.articleTitle?.richTexts ?? page.properties.title.richTexts,
-            type: .h1
-        )
-        .identifyBy(cssClass: .notion(.title))
-
         for block in page.content?.blocks ?? [] {
             AnyView(htmlBlock(for: block, with: config))
         }
