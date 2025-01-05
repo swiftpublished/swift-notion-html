e4c32bf60a421290023b6fd715ea5e00cd92b031
Shahrukh Alam <alam.shahrukh786@gmail.com>
2025-01-05 21:30:22 +0530

    marks public so that it can be used by Sponsor

--- Sources/HTML/Views/Paragraphs.swift
+++ Sources/HTML/Views/Paragraphs.swift
@@ -1,7 +1,7 @@
 import HTMLDSL
 import NotionParsing
 
-extension Paragraphs {
+public extension Paragraphs {
     init(richTexts: [NotionParsing.RichText]) {
         let richTextsElement = richTexts.map(NotionHTML.RichText.init).map(\.body).joined()
         self.init(richTextsElement)
